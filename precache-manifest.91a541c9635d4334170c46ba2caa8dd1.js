7bd489ce6baf57e5c6ca59d9afc483502355f38c
Arimazz <bodyaprymyak@icloud.com>
2020-02-17 14:00:17 +0200

    Updates

---
+++ precache-manifest.91a541c9635d4334170c46ba2caa8dd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e30995aa7d68ff0a34040480dbbe4f9",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "8893736ebaaf99083823",
+    "url": "/react_tabs/static/css/main.1487a0c3.chunk.css"
+  },
+  {
+    "revision": "b3322b73ab0fd9a1c271",
+    "url": "/react_tabs/static/js/2.c9ba2061.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_tabs/static/js/2.c9ba2061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8893736ebaaf99083823",
+    "url": "/react_tabs/static/js/main.2a919800.chunk.js"
+  },
+  {
+    "revision": "0f553968cc9d7590fcf1",
+    "url": "/react_tabs/static/js/runtime-main.414c6919.js"
+  }
+]);
